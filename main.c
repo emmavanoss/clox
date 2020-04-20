51d558f234341f305a112c815f2f296e0ab2234a
Emma Van Oss <e.vanoss@gmail.com>
2020-04-20 16:27:12 +0100

    fix double printing when running main

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 #include "vm.h"
 
 int main(int argc, const char* argv[]) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
 
   int constant = addConstant(&chunk, 1.2);
+  int constantTwo = addConstant(&chunk, 5.6);
   writeChunk(&chunk, OP_CONSTANT, 123);
-  initVM();
   writeChunk(&chunk, constant, 123);
-  writeChunk(&chunk, OP_RETURN, 123);
-  disassembleChunk(&chunk, "test chunk");
+  writeChunk(&chunk, OP_CONSTANT, 124);
+  writeChunk(&chunk, constantTwo, 124);
+  writeChunk(&chunk, OP_RETURN, 125);
+  /* disassembleChunk(&chunk, "test chunk"); */
   interpret(&chunk);
   freeVM();
   freeChunk(&chunk);
