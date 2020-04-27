fff7a4f754d8fa6c873a42ba52dab029c1eae95f
Emma Van Oss <e.vanoss@gmail.com>
2020-04-27 12:11:24 +0100

    strings have value equality

--- value.c
+++ value.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "memory.h"
+#include "object.h"
 #include "value.h"
 
 void initValueArray(ValueArray* array) {
@@ -44,5 +46,11 @@ bool valuesEqual(Value a, Value b) {
     case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
     case VAL_NIL: return true;
     case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+    case VAL_OBJ: {
+      ObjString* aString = AS_STRING(a);
+      ObjString* bString = AS_STRING(b);
+      return aString->length == bString->length &&
+        memcmp(aString->chars, bString->chars, aString->length) == 0;
+    }
   }
 }
