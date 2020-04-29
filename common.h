d87fe27a6f9c0db11790a9d38d709701ae0747a8
Emma Van Oss <e.vanoss@gmail.com>
2020-04-29 13:35:21 +0100

    compiler can compile and execute local variable declarations

--- common.h
+++ common.h
@@ -11,4 +11,5 @@
 // to print instructions as executed, with value stack
 #define DEBUG_TRACE_EXECUTION
 
+#define UINT8_COUNT (UINT8_MAX + 1)
 #endif
--- compiler.c
+++ compiler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -39,7 +40,19 @@ typedef struct {
   Precedence precedence;
 } ParseRule;
 
+typedef struct {
+  Token name;
+  int depth; // scope depth of the block where the local var was declared
+} Local;
+
+typedef struct Compiler {
+  Local locals[UINT8_COUNT]; // limited as index instruction is only 1 byte
+  int localCount;
+  int scopeDepth; // no of blocks surrounding the code curently being compiled
+} Compiler;
+
 Parser parser;
+Compiler* current = NULL;
 Chunk* compilingChunk;
 
 static Chunk* currentChunk() {
@@ -129,6 +142,12 @@ static void emitConstant(Value value) {
   emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
+static void initCompiler(Compiler* compiler) {
+  compiler->localCount = 0;
+  compiler->scopeDepth = 0;
+  current = compiler;
+}
+
 static void endCompiler() {
   emitReturn();
 #ifdef DEBUG_PRINT_CODE
@@ -136,6 +155,21 @@ static void endCompiler() {
     disassembleChunk(currentChunk(), "code");
   }
 #endif
+}
+
+static void beginScope() {
+  current->scopeDepth++;
+}
+
+static void endScope() {
+  current->scopeDepth--;
+
+  // Clean up local variables from scope
+  while (current->localCount > 0 &&
+      current->locals[current->localCount - 1].depth > current->scopeDepth) {
+    emitByte(OP_POP);       // Free up slot on stack
+    current->localCount--;  // Discard local var from array
+  }
 }
 
 static void expression();
@@ -291,8 +325,50 @@ static uint8_t identifierConstant(Token* name) {
   return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
 }
 
+static bool identifiersEqual(Token* a, Token* b) {
+  if (a->length != b->length) return false;
+  return memcmp(a->start, b->start, a->length) == 0;
+}
+
+static void addLocal(Token name) {
+  if (current->localCount == UINT8_COUNT) {
+    error("Too many local variables in function (max 256).");
+    return;
+  }
+  Local* local = &current->locals[current->localCount++];
+  local->name = name;
+  local->depth = current->scopeDepth;
+}
+
+static void declareVariable() {
+  // Global vars are late bound, so no need to keep track
+  // For local vars, need to keep track of names declared
+  if (current->scopeDepth == 0) return;
+
+  Token* name = &parser.previous;
+
+  // Check for exisiting vars with the same name in the same scope
+  for (int i = current->localCount - 1; i >= 0; i--) {
+    Local* local = &current->locals[i];
+    if (local->depth != -1 && local->depth < current->scopeDepth) {
+      break;
+    }
+
+    if (identifiersEqual(name, &local->name)) {
+      error("Variable with this name already declared in this scope.");
+    }
+  }
+
+  // add new local var to locals array
+  addLocal(*name);
+}
+
 static uint8_t parseVariable(const char* errorMessage) {
   consume(TOKEN_IDENTIFIER, errorMessage);
+
+  declareVariable();
+  if (current->scopeDepth > 0) return 0; // if in a local scope, exit
+
   return identifierConstant(&parser.previous);
 }
 
@@ -304,7 +380,17 @@ static void expression() {
   parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void defineVariable(uint8_t index) {
+  if (current->scopeDepth > 0) return; // if local, just leave value on stack
+
   emitBytes(OP_DEFINE_GLOBAL, index);
 }
 
@@ -379,6 +465,10 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_LEFT_BRACE)) {
+    beginScope();
+    block();
+    endScope();
   } else {
     expressionStatement();
   }
@@ -386,9 +476,12 @@ static void statement() {
 
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
+  Compiler compiler;
+  initCompiler(&compiler);
+  compilingChunk = chunk;
+
   parser.hadError = false;
   parser.panicMode = false;
-  compilingChunk = chunk;
   advance();
 
   while (!match(TOKEN_EOF)) {
