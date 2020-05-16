a54026af3e8cc8b8759f52990f4701b213d13a85
Emma Van Oss <e.vanoss@gmail.com>
2020-05-16 22:11:10 +0100

    ObjectFunction type exists for compiler

--- object.h
+++ object.h
@@ -2,16 +2,20 @@
 #define clox_object_h
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)     (AS_OBJ(value)->type) // returns objType
 
+#define IS_FUNCTION(value)  (isObjType(value, OBJ_FUNCTION))
 #define IS_STRING(value)    (isObjType(value, OBJ_STRING))
 
+#define AS_FUNCTION(value)  ((ObjFunction*)AS_OBJ(value))
 #define AS_STRING(value)    ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)   (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
+  OBJ_FUNCTION,
   OBJ_STRING,
 } ObjType;
 
@@ -21,6 +25,13 @@ struct sObj {
   struct sObj* next;
 };
 
+typedef struct {
+  Obj obj;
+  int arity;
+  Chunk chunk;
+  ObjString* name;
+} ObjFunction;
+
 // alias ObjString
 struct sObjString {
   Obj obj;
@@ -29,6 +40,7 @@ struct sObjString {
   uint32_t hash;
 };
 
+ObjFunction* newFunction();
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 void printObject(Value value);
